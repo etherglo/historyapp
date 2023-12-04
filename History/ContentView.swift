9e8f91c358ab418fffa2b4df3e0e08bc26c1cb9a
Akshit Anchan <69382504+akshitanchan@users.noreply.github.com>
2023-12-04 22:57:58 +0400

    basic

---
+++ History/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  History
+//
+//  Created by Akshit Anchan on 12/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ History/HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryApp.swift
+//  History
+//
+//  Created by Akshit Anchan on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
